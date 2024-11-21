a62c9540e5cc0ba6bfebc215b7b76f2527b4d956
Ethan Cornwill <ethancornwill@outlook.com>
2024-11-21 20:48:19 +1100

    Refactor PostContainer to improve date formatting and add updatePost method for inline editing

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -7,7 +7,13 @@ export default class PostContainer extends Component {
     super()
 
     const date = new Date()
-    const dateString = date.toLocaleString('default', { day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit' })
+    const dateString = date.toLocaleString('default', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    })
 
     this.state = {
       isEditing: false,
@@ -23,30 +29,54 @@ export default class PostContainer extends Component {
     this.setState({ isEditing: !this.state.isEditing })
   }
 
+  updatePost (newAuthor, newLocation, newContent) {
+    const date = new Date()
+    const dateString = date.toLocaleString('default', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    })
+
+    this.setState({
+      author: newAuthor,
+      location: newLocation,
+      content: newContent,
+      lastUpdated: dateString
+    })
+    this.toggleEditMode()
+  }
+
   render () {
     return (
-            <>
-            <button onClick={() => this.toggleEditMode()}>Toggle Edit Mode</button>
-                {
-                    this.state.isEditing
-                      ? <EditablePost
-                            // Pass all of them using props, one at a time
-                            author={this.state.author}
-                            dateCreated={this.state.dateCreated}
-                            location={this.state.location}
-                            content={this.state.content}
-                            lastUpdated={this.state.lastUpdated}
-                        />
-                      : <ReadOnlyPost
-                            // Pass all of them using props, one at a time
-                            author={this.state.author}
-                            dateCreated={this.state.dateCreated}
-                            location={this.state.location}
-                            content={this.state.content}
-                            lastUpdated={this.state.lastUpdated}
-                        />
-                }
-            </>
+      <>
+        <button onClick={() => this.toggleEditMode()}>Toggle Edit Mode</button>
+        {this.state.isEditing
+          ? (
+          <EditablePost
+            // Pass all of them using props, one at a time
+            author={this.state.author}
+            dateCreated={this.state.dateCreated}
+            location={this.state.location}
+            content={this.state.content}
+            lastUpdated={this.state.lastUpdated}
+            updatePost={(author, location, content) =>
+              this.updatePost(author, location, content)
+            }
+          />
+            )
+          : (
+          <ReadOnlyPost
+            // Pass all of them using props, one at a time
+            author={this.state.author}
+            dateCreated={this.state.dateCreated}
+            location={this.state.location}
+            content={this.state.content}
+            lastUpdated={this.state.lastUpdated}
+          />
+            )}
+      </>
     )
   }
 }
