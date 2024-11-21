d9b628dd344831b72566d229e6f91172b516698d
Ethan Cornwill <ethancornwill@outlook.com>
2024-11-21 20:48:28 +1100

    Refactor EditablePost component to manage local state for author, location, and content; add handleChange method and save button for inline editing

--- src/components/EditablePost.jsx
+++ src/components/EditablePost.jsx
@@ -5,25 +5,63 @@ export default class EditablePost extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isEditing: true
+      author: props.author,
+      location: props.location,
+      content: props.content
     }
+  }
+
+  handleChange (e) {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
   }
 
   render () {
     return (
-    <div className='EditablePost'>
-      {/* Editable Author Field */}
-      <label htmlFor='author'>Author:</label>
-      <input type='text' id='author' name='author' value={this.props.author} />
+      <div className='EditablePost'>
+        {/* Editable Author Field */}
+        <label htmlFor='author'>Author:</label>
+        <input
+          type='text'
+          id='author'
+          name='author'
+          value={this.state.author}
+          onChange={e => this.handleChange(e)}
+        />
 
-      {/* Editable Location Field */}
-      <label htmlFor='location'>Location:</label>
-      <input type='text' id='location' name='location' value={this.props.location} />
+        {/* Editable Location Field */}
+        <label htmlFor='location'>Location:</label>
+        <input
+          type='text'
+          id='location'
+          name='location'
+          value={this.state.location}
+          onChange={e => this.handleChange(e)}
+        />
 
-      {/* Editable Content Field */}
-      <label htmlFor='content'>Content:</label>
-      <input type='text' id='content' name='content' value={this.props.content} />
-    </div>
+        {/* Editable Content Field */}
+        <label htmlFor='content'>Content:</label>
+        <input
+          type='text'
+          id='content'
+          name='content'
+          value={this.state.content}
+          onChange={e => this.handleChange(e)}
+        />
+
+        {/* Save Button */}
+        <button
+          onClick={() =>
+            this.props.updatePost(
+              this.state.author,
+              this.state.location,
+              this.state.content
+            )
+          }
+        >
+          Save
+        </button>
+      </div>
     )
   }
 }
@@ -33,5 +71,6 @@ EditablePost.propTypes = {
   dateCreated: PropTypes.string.isRequired,
   lastUpdated: PropTypes.string,
   location: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired
+  content: PropTypes.string.isRequired,
+  updatePost: PropTypes.func.isRequired
 }
